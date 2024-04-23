c13b62fec27128c8f8826204e9d099f62423cab0
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2024-04-23 07:06:01 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.regex.*;
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\output.txt")); BufferedReader reader = new BufferedReader(new FileReader("D:\\input.txt"))) {
+            String check;
+            StringBuilder code = new StringBuilder();
+            while ((check = reader.readLine()) != null) {
+                code.append(check).append("\n");
+            }
+            String noComments = code.toString().replaceAll("(//.*|/\\*[\\s\\S]*?\\*/)", "");
+            writer.write(noComments);
+        } catch (IOException e) {
+            System.err.println();
+        }
+    }}
